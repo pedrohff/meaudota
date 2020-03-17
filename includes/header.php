a3433bd413ff5993f6384b13fa2109bd7a92d239
Pedro Feitosa <pedrohenriqueff@gmail.com>
2020-03-18 00:30:58 +0900

    Fixing bugs

--- includes/header.php
+++ includes/header.php
@@ -10,22 +10,22 @@ if (!isset($_SESSION)) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
-     <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+     <link rel="stylesheet" href="/assets/css/main.css">
     <title>Me Audota! :P</title>
   </head>
   <body>
     <header>
       <nav class="navbar navbar-expand-lg navbar-dark text-white bg-danger fixed-top">
         <div class="container">
-          <a class="navbar-brand" href="index.php">MeAudota!</a>
+          <a class="navbar-brand" href="/index.php">MeAudota!</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="index.php">Home</a>
+                <a class="nav-link" href="/index.php">Home</a>
               </li>
               <li class="nav-item ">
                 <a class="nav-link" href="#">Sobre</a>
@@ -34,6 +34,7 @@ if (!isset($_SESSION)) {
               <ul class="navbar-nav justify-content-end">
                   <?php
 if (isset($_SESSION['nome'])) {
+  echo "hmm";
     ?>
                     <li class="nav-item ">
                        <a class="nav-link" href="#"><?php echo 'Olá, ' . $_SESSION['nome']; ?></a>
@@ -43,6 +44,7 @@ if (isset($_SESSION['nome'])) {
                     </li>
                     <?php
 } else {
+  echo "noo";
     ?>
                   <li class="nav-item ">
                     <a class="nav-link" href="users/cadastro.php">Criar Conta</a>
--- users/cadastro.php
+++ users/cadastro.php
@@ -3,7 +3,7 @@ if (!isset($_SESSION)) {
     session_start();
 }
 $showAlert = false;
-$pdo = new PDO('mysql:dbname=login_teste;host=localhost', 'root', '');
+$pdo = new PDO('mysql:dbname=login_teste;host=localhost', 'root', 'root');
 
 if (isset($_POST['acao']) && !empty($_POST['senha'])) {
     if (strlen($_POST['senha']) < 6) {
@@ -40,51 +40,13 @@ if (isset($_POST['acao']) && !empty($_POST['senha'])) {
 }
 
 ?>
-<html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php require_once __DIR__ . '/../includes/header.php';?>
+<main>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
-     <link rel="stylesheet" href="../assets/css/main.css">
-    <title>Me Audota! :P</title>
-
-  </head>
-  <body>
-  <header>
-      <nav class="navbar navbar-expand-lg navbar-dark text-white bg-danger fixed-top">
-        <div class="container">
-          <a class="navbar-brand" href="../index.php">MeAudota!</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="../index.php">Home</a>
-              </li>
-              <li class="nav-item ">
-                <a class="nav-link" href="#">Sobre</a>
-              </li>
-            </ul>
-              <ul class="navbar-nav justify-content-end">
-                  <li class="nav-item ">
-                    <a class="nav-link" href="cadastro.php">Criar Conta</a>
-                  </li>
-                  <li class="nav-item ">
-                    <a class="nav-link" href="login.php">Entrar</a>
-                  </li>
-              </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
-
-	<section id="login">
-		<div class="container">
-			<div class="row center-block" >
-				<div class="col mb-5">
+  <section id="login">
+    <div class="container">
+      <div class="row center-block" >
+        <div class="col mb-5">
                 <?php
 if ($showAlert) {
     ?>
@@ -98,61 +60,65 @@ if ($showAlert) {
 }
 ?>
 
-					<form class="form-group" method='POST' action='<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>'>
-						<h1>Crie sua conta gratuita!</h1>
+          <form class="form-group" method='POST' action='<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>'>
+            <h1>Crie sua conta gratuita!</h1>
                         <div>
-							<label for="nome">Nome</label>
-							<input type="text" name="nome" id="nome" placeholder="Seu Nome" required class="form-control">
-						</div>
+              <label for="nome">Nome</label>
+              <input type="text" name="nome" id="nome" placeholder="Seu Nome" required class="form-control">
+            </div>
                         <div>
-							<label for="sobrenome">Sobrenome</label>
-							<input type="text" name="sobrenome" id="sobrenome" placeholder="Seu Sobrenome" required class="form-control">
-						</div>
-						<div>
-							<label for="email">E-mail</label>
-							<input type="email" name="email" id="email" placeholder="Seu E-Mail" required class="form-control">
-						</div>
-						<div>
-							<label for="senha">Senha</label>
-							<input type="password" name="senha" id="senha" placeholder="Sua Senha"  required class="form-control">
+              <label for="sobrenome">Sobrenome</label>
+              <input type="text" name="sobrenome" id="sobrenome" placeholder="Seu Sobrenome" required class="form-control">
+            </div>
+            <div>
+              <label for="email">E-mail</label>
+              <input type="email" name="email" id="email" placeholder="Seu E-Mail" required class="form-control">
+            </div>
+            <div>
+              <label for="senha">Senha</label>
+              <input type="password" name="senha" id="senha" placeholder="Sua Senha"  required class="form-control">
 
-						</div>
+            </div>
                         <div>
-							<label for="senha2">Confirme sua senha:</label>
-							<input type="password" name="senha2" id="senha2"  placeholder="Confirme sua senha" required class="form-control">
-						</div>
+              <label for="senha2">Confirme sua senha:</label>
+              <input type="password" name="senha2" id="senha2"  placeholder="Confirme sua senha" required class="form-control">
+            </div>
                         <div>
-							<label for="cep">CEP:</label>
-							<input type="number" name="cep" id="cep" placeholder="Formato 96212660 sem traços."  required class="form-control">
-						</div>
+              <label for="cep">CEP:</label>
+              <input type="number" name="cep" id="cep" placeholder="Formato 96212660 sem traços."  required class="form-control">
+            </div>
                         <div>
-							<label for="rua">Rua</label>
-							<input type="rua" name="rua" id="rua" placeholder="Rua" class="form-control">
-						</div>
+              <label for="rua">Rua</label>
+              <input type="rua" name="rua" id="rua" placeholder="Rua" class="form-control">
+            </div>
                         <div>
-							<label for="bairro">Bairro</label>
-							<input type="bairro" name="bairro" id="bairro" placeholder="Bairro" class="form-control">
-						</div>
+              <label for="bairro">Bairro</label>
+              <input type="bairro" name="bairro" id="bairro" placeholder="Bairro" class="form-control">
+            </div>
                         <div>
-							<label for="localidade">Localidade</label>
-							<input type="localidade" name="localidade" id="localidade" placeholder="Estado" class="form-control">
-						</div>
+              <label for="localidade">Localidade</label>
+              <input type="localidade" name="localidade" id="localidade" placeholder="Estado" class="form-control">
+            </div>
                         <div>
-							<label for="uf">UF</label>
-							<input type="uf" name="uf" id="uf" placeholder="UF" class="form-control">
-						</div>
-						<button type="submit" name="acao" value="Criar conta" class="btn btn-danger btn-block mt-3">Criar conta</button>
-					</form>
-				</div>
-			</div>
+              <label for="uf">UF</label>
+              <input type="uf" name="uf" id="uf" placeholder="UF" class="form-control">
+            </div>
+            <button type="submit" name="acao" value="Criar conta" class="btn btn-danger btn-block mt-3">Criar conta</button>
+          </form>
+        </div>
+      </div>
 
-		</div>
-	</section>
+    </div>
+  </section>
+</main>
+
 
 
   <!-- Adicionando JQuery -->
 
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> -->
+
+<?php require_once __DIR__ . '/../includes/footer.php';?>
 <script type="text/javascript" >
 
         $(document).ready(function() {
@@ -167,6 +133,7 @@ if ($showAlert) {
 
             //Quando o campo cep perde o foco.
             $("#cep").blur(function() {
+              console.log("cep blur");
 
                 //Nova variável "cep" somente com dígitos.
                 var cep = $(this).val().replace(/\D/g, '');
@@ -219,8 +186,10 @@ if ($showAlert) {
     </script>
   <script>
    const alertDiv = document.querySelector(".alert");
-   console.log(alertDiv);
-   setTimeout(() => {
-        alertDiv.style.display = 'none';
-   }, 5000);
+   if (alertDiv != null) {
+     setTimeout(() => {
+          alertDiv.style.display = 'none';
+     }, 5000);
+   }
+   
   </script>
